1d90b094f3972cdad6adf1cbca12ab26b5908ae7
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-04-27 19:09:57 +0300

    add migration

---
+++ migrations/20240427020110_init.up.sql
@@ -0,0 +1,9 @@
+-- Add up migration script here
+create table newsletter_subscribers (
+  email text primary key,
+  token text not null,
+  confirmed bool not null default false,
+  active bool not null default true,
+  created timestamp not null default now (),
+  updated timestamp not null default now ()
+);
